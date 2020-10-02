74a9d237201552e103816386f4fda199546bb80a
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2020-10-02 07:58:44 +0200

    webpack config - dev and prod

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: 'js/[name]-[contenthash].js',
+    path: path.resolve(__dirname, '../', 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|png|svg|gif|jpeg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name][contenthash:6].[ext]',
+            outputPath: 'images',
+          }
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              quality: 70,
+              progressive: true
+            }
+          }
+        }
+        ]
+
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ["@babel/preset-env", { useBuiltIns: 'usage', corejs: "2.0.0" }]
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties"
+          ]
+        }
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/templates/index.html",
+      title: "nowa aplikacja"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash].css'
+    }),
+    new CopyPlugin(
+      [
+        {
+          from: 'public/images',
+          to: 'images'
+        }
+      ]
+    )
+  ]
+}
