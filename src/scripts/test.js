8b1b5aeada37d5620e1dd6b8ea2fed4a0eda6589
szczepaniakmichal <mssk87@gmail.com>
2020-10-16 21:04:33 +0200

    codewars - return the highest and lowest number - ver 2

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,16 +1,8 @@
 function test(str) {
-    const numbers = str.split(' ');
-    const parseNumbers = [];
-
-    numbers.forEach(number => parseNumbers.push(parseInt(number)));
-    parseNumbers.sort((a,b) => a - b);
-
-    const highestNumber = parseNumbers[parseNumbers.length - 1];
-    const lowestNumber = parseNumbers[0];
-
-    console.log("numbers:", `"${highestNumber} ${lowestNumber}"`);
-
-    return `${highestNumber} ${lowestNumber}`;
+    let output = "";
+    const arrNumbers = str.split(' ').map(Number);
+    output += `"${Math.max(...arrNumbers) + " " + Math.min(...arrNumbers)}"`;
+    return output;
 }
 
 export { test }
