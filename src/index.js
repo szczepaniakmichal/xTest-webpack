8e130219dc316d2e984f8c6f7190390f977d37dc
szczepaniakmichal <mssk87@gmail.com>
2020-10-16 21:23:05 +0200

    codewars - return Sum of two lowest positive integers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './sass/index.scss';
 import {test} from "./scripts/test";
 
 window.onload = () => {
-    test("1 71 3 13 5 -32");
+    test([1, 71, 3, 13, 5, -32]);
 }
 
--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,8 +1,7 @@
-function test(str) {
-    let output = "";
-    const arrNumbers = str.split(' ').map(Number);
-    output += `"${Math.max(...arrNumbers) + " " + Math.min(...arrNumbers)}"`;
-    return output;
+function test(numbers) {
+    const filterNumbers = numbers.filter(number => number > 0);
+    const sortNumbers = filterNumbers.sort((a, b) => a - b);
+    return sortNumbers[0] + sortNumbers[1];
 }
 
 export { test }
