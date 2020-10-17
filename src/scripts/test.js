13dcb4e6fda0b546e04520bd4e1042c413b07cc5
szczepaniakmichal <mssk87@gmail.com>
2020-10-17 16:55:04 +0200

    codewars - Who likes it? - v3 - switch

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,15 +1,10 @@
 function test(names) {
     switch (names.length) {
-        case 0:
-            return 'no one likes this';
-        case 1:
-            return `${names[0]} likes this`;
-        case 2:
-            return `${names[0]} and ${names[1]} like this`;
-        case 3:
-            return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-        case 4:
-            return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+        case 0: return 'no one likes this';
+        case 1: return `${names[0]} likes this`;
+        case 2: return `${names[0]} and ${names[1]} like this`;
+        case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+        case 4: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
     }
 }
 
