6ae64da4d12ecb3e0a578670bb0d142cb8b511a7
szczepaniakmichal <mssk87@gmail.com>
2020-10-17 16:53:57 +0200

    codewars - Who likes it? - v2 - switch

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,12 +1,20 @@
 function test(names) {
-    if (names.length === 0) return 'no one likes this';
-    if (names.length === 1) return `${names[0]} likes this`;
-    if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
-    if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-    if (names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    switch (names.length) {
+        case 0:
+            return 'no one likes this';
+        case 1:
+            return `${names[0]} likes this`;
+        case 2:
+            return `${names[0]} and ${names[1]} like this`;
+        case 3:
+            return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+        case 4:
+            return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    }
 }
 
-document.querySelector(".test").textContent = test(["Peter"]);
+
+document.querySelector(".test").textContent = test(["Max", "John", "Mark"]);
 
 test(["Peter"]);
 test(["Jacob", "Alex"]);
