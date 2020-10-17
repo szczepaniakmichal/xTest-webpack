0732150762e6881fb41629f795ad1b2526119620
szczepaniakmichal <mssk87@gmail.com>
2020-10-17 16:46:21 +0200

    codewars - Who likes it? - v1

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,8 +1,17 @@
-function test(s) {
-    return (s.split(' ').sort((a,b) => a.length - b.length))[0].length;
+function test(names) {
+    if (names.length === 0) return 'no one likes this';
+    if (names.length === 1) return `${names[0]} likes this`;
+    if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+    if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    if (names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
 
-document.querySelector(".test").textContent = test("bitcoin take over the world maybe who knows perhaps");
+document.querySelector(".test").textContent = test(["Peter"]);
+
+test(["Peter"]);
+test(["Jacob", "Alex"]);
+test(["Max", "John", "Mark"]);
+test(["Alex", "Jacob", "Mark", "Max"]);
 
 
 export { test }
