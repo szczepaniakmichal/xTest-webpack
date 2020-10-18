d6837537c4fe4d8bc3a732c5c5caa16aaefc2544
szczepaniakmichal <mssk87@gmail.com>
2020-10-18 12:44:34 +0200

    codewars - Your order, please - v1

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,8 +1,22 @@
-function test(friends) {
-    return friends.filter(friend => friend.length === 4);
+function test(words) {
+    const sortWords = [];
+
+    const arrWords = words.split(' ');
+
+    arrWords.forEach(word => {
+        for (let i = 0; i < word.length; i++) {
+            if (word[i].match(/\d+/) !== null) {
+                sortWords[word[i].match(/\d+/)[0]] = word;
+            }
+        }
+    });
+
+    sortWords.shift();
+
+    return String(sortWords).replace(/,/g, ' ');
 }
 
-document.querySelector(".test").textContent = test(["Ryan", "Kieran", "Jason", "Yous"]);
+document.querySelector(".test").textContent = test("is2 Thi1s T4est 3a");
 
 export { test }
 
