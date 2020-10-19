dbf618982556009d9460cc6c25989f6e91361cf4
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2020-10-19 14:56:12 +0200

    clear

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,64 +1,8 @@
-function test() {
-    // helper - add dot to class name
-
-    const correctClass = document.getElementById("correctClass");
-    const resultClassName = document.getElementById("resultClassName");
-    const resultToLowerCase = document.getElementById("resultToLowerCase");
-    const copyModal = document.querySelector(".modal");
-
-    function change(e) {
-        const correctClassValue = e.target.value;
-        resultClassName.value = correctClassValue.charAt(0) === '.' ? `${correctClassValue.replace(/ /g, '.')} ` : `.${correctClassValue.replace(/ /g, '.')} `;
-        resultToLowerCase.value = correctClassValue.toLowerCase();
-    };
-
-    function copyText(selector) {
-        const copyText = document.getElementById(selector);
-        if (copyText.value.length === 0) return;
-        copyText.select();
-        copyText.setSelectionRange(0, 99999)
-        document.execCommand("copy");
-        correctClass.value = '';
-        copyModal.classList.remove('modal__hide')
-        copyModal.classList.add('modal__copy');
-        setTimeout(() => {
-            copyModal.classList.remove('modal__copy');
-            copyModal.classList.add('modal__hide');
-        }, 2000);
-    };
-
-    correctClass.addEventListener('input', change);
-
-    resultClassName.addEventListener('click', () => copyText("resultClassName"));
-    resultToLowerCase.addEventListener('click', () => copyText("resultToLowerCase"));
-
-
-
-
-    // check is string equal??
-
-    const checkString1 = document.querySelector('.check-string-1');
-    const checkString2 = document.querySelector('.check-string-2');
-    const btnCheckString = document.querySelector('.btn-check-string');
-
-    const isStrEqual = (firstStr, secondStr, el) => {
-        el.textContent = 'Are strings is equal?'
-        // const str1 = String(firstStr.value).valueOf();
-        // const str2 = String(secondStr.value).valueOf();
-        const str1 = firstStr.value;
-        const str2 = secondStr.value;
-        if (str1 === '' || str2 === '') return ;
-        let btnText =  btnCheckString.textContent;
-        if (str1 === str2) {
-            el.textContent = `${btnText} yes`
-        } else if (str1 !== str2) {
-            el.textContent = `${btnText} no`
-        } else {
-            el.textContent = `${btnText} error`
-        }
-    }
-    btnCheckString.addEventListener('click', () => isStrEqual(checkString1, checkString2, btnCheckString))
+function test(text) {
+    console.log(text)
 }
+
+test("abc")
 
 
 export { test }
