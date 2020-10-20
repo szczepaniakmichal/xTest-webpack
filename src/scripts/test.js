3e2d293e0312c9e6f1ded2f058c416e4ea3907fa
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2020-10-20 09:21:14 +0200

    codewars - Replace With Alphabet Position - v1

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,9 +1,16 @@
 function test(text) {
-    console.log(text)
+    let resultStr = '';
+    for (let i = 0; i < text.length; i++) {
+        const currentValue = text.toLowerCase().charCodeAt(i) - 96;
+        if (currentValue > 0 && currentValue < 27) resultStr += " " + currentValue;
+    }
+    return resultStr.trim();
 }
 
-test("abc")
+document.querySelector(".test").textContent = test('h/:&2 }*');
 
+// test("The sunset sets at twelve o' clock.")
 
 export { test }
 
+// return text.toLowerCase().split("").filter(el => (/[a-z]/g.test(el))).map(a => a.charCodeAt(0)-96).join(" ");
