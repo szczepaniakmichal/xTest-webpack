6101410f26aa3f64648ce9a6744bd4a4b1bca57b
Michal Szczepaniak <michal.szczepaniak@inspirelabs.pl>
2020-10-20 18:43:07 +0200

    codewars - Replace With Alphabet Position - v2

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,16 +1,13 @@
 function test(text) {
-    let resultStr = '';
-    for (let i = 0; i < text.length; i++) {
-        const currentValue = text.toLowerCase().charCodeAt(i) - 96;
-        if (currentValue > 0 && currentValue < 27) resultStr += " " + currentValue;
-    }
-    return resultStr.trim();
+    return text.toLowerCase().split('').filter(char => (/[a-z]/g.test(char))).map(properSing => properSing.charCodeAt()-96).join(' ');
 }
 
-document.querySelector(".test").textContent = test('h/:&2 }*');
+document.querySelector(".test").textContent = test('htr/r:7M&2 }*');
 
-// test("The sunset sets at twelve o' clock.")
+test("The sunset sets at twelve o' clock.")
 
 export { test }
 
+
+// a.charCodeAt(0)-96) - why is 0 in brackets?
 // return text.toLowerCase().split("").filter(el => (/[a-z]/g.test(el))).map(a => a.charCodeAt(0)-96).join(" ");
