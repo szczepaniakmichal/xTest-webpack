8fd0bf77dc8dd23782cdbf14a445377acf26cd09
szczepaniakmichal <mssk87@gmail.com>
2020-10-23 22:39:20 +0200

    clean

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,14 +1,5 @@
 function test() {
-    const parent = document.querySelector("body > div.top-bar > ul > li:nth-child(2)");
-    parent.addEventListener('mouseover', () => {
-        console.log("asfas")
-        document.querySelector("body > div.top-bar > ul > li:nth-child(2) > ul").style.display = 'block';
-    })
 
-    parent.addEventListener('mouseleave', () => {
-        console.log("asfas")
-        document.querySelector("body > div.top-bar > ul > li:nth-child(2) > ul").style.display = 'none';
-    })
 }
 
 // document.querySelector(".test").textContent = test('htr/r:7M&2 }*');
