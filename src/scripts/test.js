44ac5a595dc6f89eb6c4bfdacd4447a46149f2ae
szczepaniakmichal <mssk87@gmail.com>
2020-10-24 19:39:56 +0200

    codewars - Sum of Digits / Digital Root v5

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -2,14 +2,19 @@
 //     return (n - 1) % 9 + 1;
 // }
 
+// function test(n) {
+//     if (n < 10) return n;
+//     return test(
+//         n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+//     // whats means + +d
+// }
+
 function test(n) {
-    if (n < 10) return n;
-    return test(
-        n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
-    // whats means + +d
+    return--n%9+1;
+    //whats means --
 }
 
-document.querySelector(".test").textContent = test(123);
+document.querySelector(".test").textContent = test(58);
 test(9007199254740991);
 
 export { test }
