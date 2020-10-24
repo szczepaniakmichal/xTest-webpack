a01e4616d141df8cfc2161c2056f8d91c6c1101b
szczepaniakmichal <mssk87@gmail.com>
2020-10-24 19:55:37 +0200

    clean

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,17 +1,5 @@
-// function test(n) {
-//     return (n - 1) % 9 + 1;
-// }
+function test() {
 
-// function test(n) {
-//     if (n < 10) return n;
-//     return test(
-//         n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
-//     // whats means + +d
-// }
-
-function test(n) {
-    return--n%9+1;
-    //whats means --
 }
 
 document.querySelector(".test").textContent = test(58);
@@ -20,5 +8,7 @@ test(9007199254740991);
 export { test }
 
 
+// --n
+// + +d
 // a.charCodeAt(0)-96) - why is 0 in brackets?
 // return text.toLowerCase().split("").filter(el => (/[a-z]/g.test(el))).map(a => a.charCodeAt(0)-96).join(" ");
