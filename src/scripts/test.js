dcecf16393c9b697e9ce199a7343371c51ceb80f
szczepaniakmichal <mssk87@gmail.com>
2020-10-25 09:57:14 +0100

    codewars - Square Every Digit

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,13 +1,17 @@
-function test() {
-
+function test(num) {
+    return Number(num.toString().split('').map(digit => Math.pow(digit, 2)).join(''));
 }
 
-document.querySelector(".test").textContent = test(58);
-test(9007199254740991);
+document.querySelector(".test").textContent = test(123456789);
+// test(9007199254740991);
 
 export { test }
 
+//Square Every Digit
 
+
+// const squareDigits = (num) => Number((num + '').split("").map(c => c *c).join(""));
+// return +String(num).split('').map(function(num){return +num * +num;}).join('');
 // --n
 // + +d
 // a.charCodeAt(0)-96) - why is 0 in brackets?
