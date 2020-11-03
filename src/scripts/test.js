553e7811803220a34d5614c6e5ab301960782037
szczepaniakmichal <mssk87@gmail.com>
2020-11-03 22:02:42 +0100

    codewars - Dubstep - songDecoder(song)

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,19 +1,11 @@
-function test(n) {
-    let number = 1;
-    let value = 1;
-    if (n === 0) return n.toFixed(2);
-    if (n > 0) {
-        for (let i = 1; i < n; i++) {
-            value += 3;
-            number += 1 / value;
-        }
-        return String(number.toFixed(2));
-    }
+function test(song) {
+    return song.replace(/WUB/gi,  ' ').replace(/ +/g, ' ').trim();
 }
 
-document.querySelector(".test").textContent = test(5);
-test(2);
-test(5);
+document.querySelector(".test").textContent = test("AWUBBWUBC");
+test("AWUBBWUBC");
+test("AWUBWUBWUBWUBWUBWUBBWUBWUBWUBC");
+test("WUBAWUBBWUBCWUB");
 
 export { test }
 
@@ -26,3 +18,4 @@ export { test }
 // + +d
 // a.charCodeAt(0)-96) - why is 0 in brackets?
 // return text.toLowerCase().split("").filter(el => (/[a-z]/g.test(el))).map(a => a.charCodeAt(0)-96).join(" ");
+// regEx - find space or multiple spaces  /  +/g
