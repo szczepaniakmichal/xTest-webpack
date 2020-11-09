fc361c895f6b04734491bef833fa622ef95bade7
szczepaniakmichal <mssk87@gmail.com>
2020-11-09 22:35:09 +0100

    codewars - The Supermarket Queue

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './sass/index.scss';
 import {test} from "./scripts/test";
 
 window.onload = () => {
-    // test();
+    // test([10,2,3,3], 2);
+    // test([10,2,3,3,1,2,3,4,5], 7);
 }
 
--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,11 +1,17 @@
-function test(song) {
-    return song.replace(/WUB/gi,  ' ').replace(/ +/g, ' ').trim();
+function test(customers, n) {
+    const cashbox = [];
+    for (let i = 0; i < n; i++) {
+       cashbox.push(0)
+   }
+    customers.forEach(customer => {
+        cashbox[0] += customer;
+        cashbox.sort((x, y) => x - y)
+    })
+    return cashbox[cashbox.length - 1];
 }
 
-document.querySelector(".test").textContent = test("AWUBBWUBC");
-test("AWUBBWUBC");
-test("AWUBWUBWUBWUBWUBWUBBWUBWUBWUBC");
-test("WUBAWUBBWUBCWUB");
+// document.querySelector(".test").textContent = test("AWUBBWUBC");
+test([2,3,7,1], 3);
 
 export { test }
 
@@ -19,3 +25,4 @@ export { test }
 // a.charCodeAt(0)-96) - why is 0 in brackets?
 // return text.toLowerCase().split("").filter(el => (/[a-z]/g.test(el))).map(a => a.charCodeAt(0)-96).join(" ");
 // regEx - find space or multiple spaces  /  +/g
+
